827a68ebfcdded1be23d934d67b612ee53df8d65
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-10-05 13:24:36 +0200

    2

--- Embedded/Projet-Robot_Hanna_Capolino.X/os5C.c
+++ Embedded/Projet-Robot_Hanna_Capolino.X/os5C.c
@@ -123,7 +123,7 @@ void SetNextRobotStateInAutomaticMode5c() {
             nextStateRobot = STATE_AVANCE;
             break;
         case 0b00001:
-            nextStateRobot = STATE_TOURNE_GAUCHE;
+            nextStateRobot = STATE_LEGET_TOURNE_GAUCHE;
             break;
         case 0b00010:
             nextStateRobot = STATE_TOURNE_GAUCHE;
@@ -147,37 +147,37 @@ void SetNextRobotStateInAutomaticMode5c() {
             nextStateRobot = STATE_TOURNE_DROITE;
             break;
         case 0b01001:
-            nextStateRobot = STATE_TOURNE_DROITE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
             break;
         case 0b01010:
             nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b01011:
-            nextStateRobot = STATE_TOURNE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b01100:
             nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
             break;
         case 0b01101:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b01110:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b01111:
             nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
             break;
         case 0b10000:
-            nextStateRobot = STATE_TOURNE_DROITE;
+            nextStateRobot = STATE_LEGET_TOURNE_DROITE;
             break;
         case 0b10001:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b10010:
-            nextStateRobot = STATE_TOURNE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
             break;
         case 0b10011:
-            nextStateRobot = STATE_TOURNE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
             break;
         case 0b10100:
             nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
@@ -189,7 +189,7 @@ void SetNextRobotStateInAutomaticMode5c() {
             nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
             break;
         case 0b10111:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b11000:
             nextStateRobot = STATE_TOURNE_DROITE;
@@ -207,7 +207,7 @@ void SetNextRobotStateInAutomaticMode5c() {
             nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
             break;
         case 0b11101:
-            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
+            nextStateRobot = STATE_TOURNE_SUR_PLACE;
             break;
         case 0b11110:
             nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
