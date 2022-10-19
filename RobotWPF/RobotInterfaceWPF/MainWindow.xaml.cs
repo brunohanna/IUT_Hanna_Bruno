9bb9794ef8a03490d8ecae2728dee871a560d47f
Robot <touloniut@gmail.com>
2022-10-19 10:57:05 +0200

    Update MainWindow.xaml.cs

--- RobotWPF/RobotInterfaceWPF/MainWindow.xaml.cs
+++ RobotWPF/RobotInterfaceWPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RobotInterface
+namespace RobotInterfaceWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
